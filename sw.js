6bb7eec557285c13c0eb750c2e2f08963ff23755
Serdar <serdar@Serdar-Dizustu-Bilgisayar.local>
2026-02-07 11:58:17 +0300

    Update sw.js Firebase version to 11.0.2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging-compat.js');
 
 const CACHE_NAME = 'spor-app-v10'; // Incremented version
 const ASSETS = [
